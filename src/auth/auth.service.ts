88c1319d17bbcb29dc2f09100d26f7d8d8f0452a
rojiwon0325 <bond9986@naver.com>
2022-05-12 22:38:44 +0900

    authService authenticate validation추가 그냥 해당 서비스함수 내에서 검증 로직을 추가하였습니다.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,13 +3,23 @@ import { Injectable } from '@nestjs/common';
 import { UserEntity } from 'src/users/user.entity';
 import { AuthenticateUserDTO } from 'src/users/users.dto';
 import { UsersService } from 'src/users/users.service';
+import { validate } from 'class-validator';
+import { HttpExceptionService } from 'src/httpException/httpException.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly httpExceptionService: HttpExceptionService,
+  ) {}
 
-  authenticate(dto: AuthenticateUserDTO): Promise<UserEntity> {
+  async authenticate(dto: AuthenticateUserDTO): Promise<UserEntity> {
     const data = plainToInstance(AuthenticateUserDTO, dto);
+    const errors = await validate(data);
+    for (const error of errors) {
+      const message = Object.entries(error.constraints)[0][1];
+      throw this.httpExceptionService.getBadRequestException(message);
+    }
     return this.usersService.findAuthenticatedUser(data);
   }
 }
