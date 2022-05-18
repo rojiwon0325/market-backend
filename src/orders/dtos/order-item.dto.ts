31675160a139846df31dd1fb904f277117403375
rojiwon0325 <bond9986@naver.com>
2022-05-18 16:38:57 +0900

    불필요한 dto 제거

--- src/orders/dtos/order-item.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PickType } from '@nestjs/swagger';
-import { OrderItemEntity } from '../entities/order-item.entity';
-
-export class OrderItem extends PickType(OrderItemEntity, [
-  'uid',
-  'product_id',
-  'product_price',
-  'count',
-]) {}
