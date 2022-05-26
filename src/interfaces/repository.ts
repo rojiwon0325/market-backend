9f7aa6a49f79592a6ab9c51b848b4511f78f4d6e
rojiwon0325 <bond9986@naver.com>
2022-05-26 16:00:07 +0900

    레포지토리 인터페이스 정의 - 조회, 수정 인터페이스 정의 - 두 인터페이스를 합친 base repository 클래스 정의 - 해당 클래스를 implements한 추상화 클래스 정의  => 공통 코드 작성

---
+++ src/interfaces/repository.ts
@@ -0,0 +1,91 @@
+import { ClassConstructor, plainToInstance } from 'class-transformer';
+import { Document, FilterQuery, Model } from 'mongoose';
+
+export type DeleteResult = {
+  acknowledged: boolean;
+  deletedCount: number;
+};
+
+export type FindParameter<M, T> = {
+  filter?: FilterQuery<M & Document>;
+  cls: ClassConstructor<T>;
+};
+export type FindOneParameter<M, T> = {
+  filter: FilterQuery<M & Document>;
+  cls: ClassConstructor<T>;
+};
+
+export type UpdateOneParameter<M> = {
+  filter: FilterQuery<M & Document>;
+  data: Partial<M>;
+};
+
+export interface IRepositoryRead<M> {
+  find: <T>(parameter: FindParameter<M, T>) => Promise<T[]>;
+  findOne: <T>(parameter: FindOneParameter<M, T>) => Promise<T>;
+  count: (filter?: FilterQuery<M & Document>) => Promise<number>;
+}
+export interface IRepositoryWrite<M> {
+  create: (data: Partial<M>) => Promise<M>;
+  updateOne: (parameter: UpdateOneParameter<M>) => Promise<M>;
+  deleteOne: (filter: FilterQuery<M & Document>) => Promise<DeleteResult>;
+}
+
+export declare class IBaseRepository<M>
+  implements IRepositoryWrite<M>, IRepositoryRead<M>
+{
+  find: <T>(parameter: FindParameter<M, T>) => Promise<T[]>;
+  findOne: <T>(parameter: FindOneParameter<M, T>) => Promise<T>;
+  count: (filter?: FilterQuery<M & Document>) => Promise<number>;
+
+  create: (data: Partial<M>) => Promise<M>;
+  updateOne: (parameter: UpdateOneParameter<M>) => Promise<M>;
+  deleteOne: (filter: FilterQuery<M & Document>) => Promise<DeleteResult>;
+}
+
+export abstract class BaseRepository<M> implements IBaseRepository<M> {
+  constructor(
+    protected readonly model: Model<M & Document>,
+    private readonly cls: ClassConstructor<M>,
+  ) {}
+
+  async find<T>({ filter, cls }: FindParameter<M, T>): Promise<T[]> {
+    const list = (await this.model.find(filter)).map((one) => one.toObject());
+    return plainToInstance(cls, list, { strategy: 'excludeAll' });
+  }
+  async findOne<T>({ filter, cls }: FindOneParameter<M, T>): Promise<T> {
+    const one = await this.model.findOne(filter);
+    if (one) {
+      return plainToInstance(cls, one.toObject(), { strategy: 'excludeAll' });
+    } else {
+      return undefined;
+    }
+  }
+  async count(filter?: FilterQuery<M & Document>): Promise<number> {
+    return this.model.count(filter);
+  }
+  async create(data: Partial<M>): Promise<M> {
+    const one = await this.model.create(data);
+    return plainToInstance(this.cls, one.toObject());
+  }
+
+  async updateOne({ filter, data }: UpdateOneParameter<M>): Promise<M> {
+    const one = await this.model.findOne(filter);
+    if (one) {
+      await this.model.updateOne(filter, data);
+      return plainToInstance(
+        this.cls,
+        {
+          ...one.toObject(),
+          ...data,
+        },
+        { strategy: 'excludeAll' },
+      );
+    } else {
+      return undefined;
+    }
+  }
+  async deleteOne(filter: FilterQuery<M & Document>): Promise<DeleteResult> {
+    return this.model.deleteOne(filter);
+  }
+}
