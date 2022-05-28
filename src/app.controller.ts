0a6a4086c2aeeae7b9d76b6ec7887e621fe571f4
rojiwon0325 <bond9986@naver.com>
2022-05-28 18:26:29 +0900

    home api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
 import { Controller, Get } from '@nestjs/common';
 import { Public } from './auth/Public.decorator';
+import { ProductSimple } from './products/entities/product.simple';
+import { ProductsService } from './products/products.service';
 
 @Public()
 @Controller()
 export class AppController {
+  constructor(private readonly productsService: ProductsService) {}
+
   @Get()
   default() {
     return 'Welcome To E-Market';
+  }
+  @Get('home')
+  async home() {
+    const products = await this.productsService.find({ cls: ProductSimple });
+    return [
+      {
+        title: '이 상품 어때요?',
+        products,
+      },
+      {
+        title: '지금 가장 핫한 상품',
+        products,
+      },
+    ];
   }
   @Get('health-check')
   check() {
